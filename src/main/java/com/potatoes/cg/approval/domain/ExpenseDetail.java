9a93d66d01ab8d28b67da980637723cfc8e65dbf
tinypig705 <tinypig705@naver.com>
2023-12-01 14:11:18 +0900

    Feature/#17/기안서작성 - 지출결의서 완성

--- src/main/java/com/potatoes/cg/approval/domain/ExpenseDetail.java
+++ src/main/java/com/potatoes/cg/approval/domain/ExpenseDetail.java
@@ -3,9 +3,13 @@ package com.potatoes.cg.approval.domain;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "tbl_expense_detail")
@@ -18,7 +22,34 @@ public class ExpenseDetail {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long expenseDetailCode;
 
-//    @Column
-//    @NotNull
-//    private String exp
+    @Column(nullable = false)
+    private String expenseAccount;
+
+    @Column(nullable = false)
+    @DateTimeFormat(pattern = "YYYY-MM-DD")
+    private LocalDate expenseDate;
+
+    @Column
+    private Long expensePrice;
+
+    @Column
+    private String expenseBriefs;
+
+    public ExpenseDetail(String expenseAccount, LocalDate expenseDate, Long expensePrice, String expenseBriefs) {
+        this.expenseAccount = expenseAccount;
+        this.expenseDate = expenseDate;
+        this.expensePrice = expensePrice;
+        this.expenseBriefs = expenseBriefs;
+    }
+
+    public static ExpenseDetail of(String expenseAccount, LocalDate expenseDate, Long expensePrice, String expenseBriefs) {
+        return new ExpenseDetail(
+                expenseAccount,
+                expenseDate,
+                expensePrice,
+                expenseBriefs
+        );
+    }
+
+
 }
--- src/main/java/com/potatoes/cg/approval/presentation/ApprovalController.java
+++ src/main/java/com/potatoes/cg/approval/presentation/ApprovalController.java
@@ -1,9 +1,11 @@
 package com.potatoes.cg.approval.presentation;
 
+import com.potatoes.cg.approval.dto.request.ExpenseCreateRequest;
 import com.potatoes.cg.approval.dto.request.LetterCreateRequest;
 import com.potatoes.cg.approval.service.ApprovalService;
 import com.potatoes.cg.jwt.CustomUser;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +18,7 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/cg-api/v1/approval")
+@Slf4j
 public class ApprovalController {
 
     private final ApprovalService approvalService;
@@ -33,12 +36,17 @@ public class ApprovalController {
     }
 
     /* 기안서 작성 - 지출결의서 */
-//    @PostMapping("/expense")
-//    public ResponseEntity<Void> expenseSave(@RequestPart @Valid final expenseCreateRequest expenseRequest,
-//                                            @AuthenticationPrincipal CustomUser customUser,
-//                                            @RequestPart(required = false) final List<MultipartFile> attachment) {
-//
-//
-//        return null;
-//    }
+    @PostMapping("/expense")
+    public ResponseEntity<Void> expenseSave(@RequestPart(required = false) @Valid final ExpenseCreateRequest expenseRequest,
+                                            @AuthenticationPrincipal CustomUser customUser,
+                                            @RequestPart(required = false) final List<MultipartFile> attachment) {
+
+        log.info("expenseRequest : {}",expenseRequest);
+
+        final Long expenseCode = approvalService.expenseSave(expenseRequest, attachment , customUser);
+
+        return ResponseEntity.created(URI.create("/expense/" + expenseCode)).build();
+
+
+    }
 }
